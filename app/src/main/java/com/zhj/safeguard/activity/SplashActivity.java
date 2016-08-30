c48396bfa9aac56dae4336fe4a3b18bf35956380
zhj <960116284@qq.com>
2016-08-31 01:04:27 +0800

    使用xutils的download下载最新apk.

--- app/src/main/java/com/zhj/safeguard/activity/SplashActivity.java
+++ app/src/main/java/com/zhj/safeguard/activity/SplashActivity.java
@@ -2,14 +2,21 @@ package com.zhj.safeguard.activity;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.zhj.safeguard.R;
 import com.zhj.safeguard.utils.PackageInfoUtils;
 
@@ -22,6 +29,7 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 
 public class SplashActivity extends Activity {
@@ -29,6 +37,8 @@ public class SplashActivity extends Activity {
     private static final String TAG = "SplashActivity";
     private static final int SHOW_UPDATE_DIALOG = 100;
     private TextView mTvVersion;
+    private String mDesc; //描述信息
+    private String mUrl; //最新安装包地址
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -42,6 +52,7 @@ public class SplashActivity extends Activity {
 
         }
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +84,8 @@ public class SplashActivity extends Activity {
         public void run() {
             //1.去服务器获取最新版本
             String uri = "http://192.168.56.1:8080/update.json";
+            /*---------------update.json---------------------*/
+            //{"versionCode":2,"desc":"修复了bug","url":"http://192.168.56.1:8080/safe2.apk"}
             //高版本SDK api23不能直接使用HttpClient,需导入org.apache.http.legacy.jar
 
 //            /*----------------方式一--------------------*/
@@ -113,6 +126,8 @@ public class SplashActivity extends Activity {
                     //解析json
                     JSONObject jsonObj = new JSONObject(json);
                     int netCode = jsonObj.getInt("versionCode");//服务器最新版本号
+                    mDesc = jsonObj.getString("desc");
+                    mUrl = jsonObj.getString("url");
                     //本地版本号
                     int localcode = PackageInfoUtils.getVersionCode(SplashActivity.this);
 
@@ -141,10 +156,53 @@ public class SplashActivity extends Activity {
 
     private void showUpdateDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);  //v4
+        builder.setCancelable(false); //点击旁边不可取消
         builder.setTitle("版本更新提醒");//标题
-        builder.setMessage("增加了功能"); //服务器获取
-        builder.setPositiveButton("立刻升级", null);
-        builder.setNegativeButton("稍后再说", null);
+        builder.setMessage(mDesc); //服务器获取,描述信息
+        builder.setPositiveButton("立刻升级", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //下载最新版本,提示安装
+
+
+            }
+        });
+        builder.setNegativeButton("稍后再说", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                load2home();
+            }
+        });
         builder.show();
     }
+
+    //下载最新版本
+    private void downloadNewVersion() {
+        //弹出diolog下载apk
+        final ProgressDialog dialog1 = new ProgressDialog(this);
+        dialog1.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL); //设置样式
+        dialog1.setCancelable(false);
+        dialog1.show();
+        //下载apk
+        HttpUtils utils = new HttpUtils();
+        String apkName = System.currentTimeMillis() + ".apk"; //文件名称
+        String target = new File(Environment.getExternalStorageDirectory(),
+                apkName).getAbsolutePath(); //下载后文件存放的本地地址
+        utils.download(mUrl, target, new RequestCallBack<File>() {
+            @Override
+            public void onSuccess(ResponseInfo<File> responseInfo) {
+                //下载成功后的回调
+                dialog1.dismiss();//消失
+                
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+
+                dialog1.dismiss();
+                load2home();
+            }
+        });
+    }
+
 }
